694b221b7b45023d247213eee8bad0401273e582
Tokutaro Shimada <tokutaro@gmail.com>
2017-01-08 15:53:21 +0900

    fixed what tslint pointed out.

--- web-client-sample/src/main/ts/example-async.ts
+++ web-client-sample/src/main/ts/example-async.ts
@@ -1,14 +1,12 @@
-async function main()
-{
+async function main() {
   console.log("waiting 1sec.");
   await scheduleMsec(1000);
   console.log("done.");
 }
 
-function scheduleMsec(msec: number)
-{
-  return new Promise( (resolve,reject) => setTimeout(function() { resolve(); }, msec) );
-//return new Promise( resolve => setTimeout(resolve, msec) );
+function scheduleMsec(msec: number) {
+  return new Promise( (resolve, reject) => setTimeout(function() { resolve(); }, msec) );
+// return new Promise( resolve => setTimeout(resolve, msec) );
 }
 
 main();
