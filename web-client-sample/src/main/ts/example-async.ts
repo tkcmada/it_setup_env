27e021e1eaaf8db7850954accbfdfba699ee2b70
Tokutaro Shimada <tokutaro@gmail.com>
2017-01-08 15:32:21 +0900

    added babel-polyfill to use Typescript-async/await feature.

---
+++ web-client-sample/src/main/ts/example-async.ts
@@ -0,0 +1,14 @@
+async function main()
+{
+  console.log("waiting 1sec.");
+  await scheduleMsec(1000);
+  console.log("done.");
+}
+
+function scheduleMsec(msec: number)
+{
+  return new Promise( (resolve,reject) => setTimeout(function() { resolve(); }, msec) );
+//return new Promise( resolve => setTimeout(resolve, msec) );
+}
+
+main();
