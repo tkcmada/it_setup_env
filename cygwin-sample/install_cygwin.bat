eb7b1f167d8af04fbd581b79bd53b311bcfe028b
Tokutaro Shimada <tokutaro@gmail.com>
2017-01-14 17:43:15 +0900

    remove cygwin-sample.

--- cygwin-sample/install_cygwin.bat
+++
@@ -1,1 +0,0 @@
-%~dp0bin\setup-x86_64.exe --packages python3,curl,make --quiet-mode --no-startmenu --no-desktop
--- cygwin-sample/targz_cygwin64_dir.bat
+++
@@ -1,7 +0,0 @@
-@IF "%CYGWIN_HOME%" == "" (
-  set CYGWIN_HOME=c:\cygwin64
-)
-set PATH=%CYGWIN_HOME%\bin;%PATH%
-cd %~dp0
-tar zcvf cygwin64.tar.gz "%CYGWIN_HOME%"
-pause
