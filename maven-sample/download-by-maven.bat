c60149ea449367e8ca92a2587ccb145479dfea6b
toukei <toukei1985@gmail.com>
2017-02-07 02:31:42 +0900

    Update download-by-maven.bat
    modify batch

--- maven-sample/download-by-maven.bat
+++ maven-sample/download-by-maven.bat
@@ -5,8 +5,9 @@ cd %~dp0
 call setenv.bat
 
 @rem call is require to prevent window from closing.
-@rem call mvn -e -X compile
 call mvn dependency:go-offline
+@rem dont use compile. because it's cannot detect error.
+@rem call mvn -e -X compile
 IF ERRORLEVEL 1 (
   @echo error occured
   pause
