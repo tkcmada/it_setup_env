c0ec2a7dd565a242bd9420a3975e29debbfaf430
Tokutaro Shimada <tokutaro@gmail.com>
2017-06-30 00:34:05 +0900

    added jar to maven. fst-serialization.

---
+++ docker-sample/sample-node.js-1/build.bat
@@ -0,0 +1,2 @@
+docker build -t hello:1.0 .
+pause
--- maven-sample/make_zip.bat
+++ maven-sample/make_zip.bat
@@ -1,5 +1,6 @@
 @echo off
 cd %~dp0
+del /Q repository.jar.*
 "%~dp0..\7-zip\7z.exe" a -tzip -v49m repository.jar repository
 IF ERRORLEVEL 1 (
   echo error occured
